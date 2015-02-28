5110546ca456e90d4146bd88455b922a552f9b28
Stephan Genyk <sgenyk91@gmail.com>
2015-02-28 13:49:31 -0800

    Solves anarchy

---
+++ anarchy.js
@@ -0,0 +1,10 @@
+/* CHALLNEGE:
+* Find a way to return 'I can write numbers, 1, 2, 3' without using numbers or length
+*/
+
+var anarchy = function () {
+  var one = Math.PI / Math.PI;
+  var two = one + one;
+  var three = two + one;
+  return 'I can write numbers like, ' + one + ', ' + two + ', ' + three + '.';
+}
