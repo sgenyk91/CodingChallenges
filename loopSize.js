a8bd3b0781718542af5957dd302354f236943f5f
Stephan Genyk <sgenyk91@gmail.com>
2015-03-02 01:35:30 -0800

    Solves loopSize

--- loopSize.js
+++ loopSize.js
@@ -1,6 +1,16 @@
+/* CHALLNEGE:
+* Find the length of the loop of a linked list that contains a tail and loop
+*/
 
-
-function loop_size(node){
-  console.log(node);
-  console.log(node.getNext());
+function loop_size(node) {
+  var counter = 0;
+  node['test'] = node.test ? !node.test : true;
+  node['count'] = counter;
+  while (node.test !== node.next.test) {
+    node.next.test = node.test;
+    node = node.next;
+    node['count'] = ++counter;
+  }
+  var loopSize = node.count - node.next.count;
+  return loopSize;
 }
