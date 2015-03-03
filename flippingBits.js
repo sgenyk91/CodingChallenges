619472f0b1cddf0578cda18af0c25e806e248e9f
Stephan Genyk <sgenyk91@gmail.com>
2015-03-03 15:11:58 -0800

    Solves flippingBits

--- flippingBits.js
+++ flippingBits.js
@@ -1,1 +1,20 @@
-flippingBits.js
+function processData(input) {
+  input = input.split('\n').slice(1);
+  var result = [];
+  for (var i = 0; i < input.length; i++) {
+    var binary = (parseInt(input[i])).toString(2);
+    while (binary.length < 32) {
+      binary = '0' + binary;
+    }
+    var binaryFlip = '';
+    for (var j = 0; j < binary.length; j++) {
+      if (binary[j] === '0') {
+        binaryFlip += '1';
+      } else {
+        binaryFlip += '0';
+      }
+    }
+    result.push(parseInt(binaryFlip, 2));
+  }
+  console.log(result.join('\n'));
+}
