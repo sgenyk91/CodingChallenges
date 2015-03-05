92bc4714decc664116255fbb3dd6691f7dfa8d84
Stephan Genyk <sgenyk91@gmail.com>
2015-03-04 18:05:53 -0800

    Solves pickPeaks

--- paginationHelper.js
+++ paginationHelper.js
@@ -3,7 +3,6 @@
 // The constructor takes in an array of items and a integer indicating how many
 // items fit within a single page
 function PaginationHelper(collection, itemsPerPage){
-  console.log("COLLECTION: %s / ITEMS PER PAGE: %s", collection, itemsPerPage);
   this.collection = collection;
   this.itemsPerPage = itemsPerPage;
 }
@@ -25,24 +24,20 @@ PaginationHelper.prototype.pageCount = function() {
 // returns the number of items on the current page. page_index is zero based.
 // this method should return -1 for pageIndex values that are out of range
 PaginationHelper.prototype.pageItemCount = function(pageIndex) {
-  console.log("PAGE INDEX: ", pageIndex);
   var pagesWithFullCount = Math.floor(this.collection.length / this.itemsPerPage) - 1;
   if (pagesWithFullCount + 1 < pageIndex) {
     return -1;
   }
   if (pageIndex <= pagesWithFullCount) {
-    console.log('HERE');
     return this.itemsPerPage;
   }
   var remainder = this.collection.length % this.itemsPerPage;
-  console.log(remainder);
   return remainder;
 }
 
 // determines what page an item is on. Zero based indexes
 // this method should return -1 for itemIndex values that are out of range
 PaginationHelper.prototype.pageIndex = function(itemIndex) {
-  console.log("ITEM INDEX: ", itemIndex);
   if (this.collection.length - 1 < itemIndex || itemIndex < 0) {
     return -1;
   }
